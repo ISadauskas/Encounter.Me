3b442e6e1f3d3c920831c1dc4b48c5434a0889cd
ISadauskas <ignas.sadauskas7@gmail.com>
2021-10-17 12:15:14 +0300

    Fixed Conflicts/Added interfaces

--- BusinessLogic/Trail.cs
+++ BusinessLogic/Trail.cs
@@ -1,13 +1,14 @@
 ﻿using BusinessLogic;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace EncounterMeWF
 {
     [Serializable]
-    public class Trail
+    public class Trail : IEquatable<Trail>
     {
-        
+
         public int[] IndexID = new int[1];
         public int this[int index]
         {
@@ -15,42 +16,47 @@ namespace EncounterMeWF
             set => IndexID[index] = value;
         }
 
+        public bool Equals(Trail other)
+        {
+            if (other == null)
+                return false;
+
+            if (this.ID == other.ID)
+                return true;
+            else
+                return false;
+        }
+
+        public bool CheckTrailID(BindingList<Trail> TrailList, Trail tempTrail)
+        {
+            foreach (var item in TrailList)
+            {
+                if (tempTrail.Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
 
         public int ID { get; set; }
         public string Name { get; set; }
-
-        private double length;
-        public double Length
-        {
-            get
-            {
-                return this.length;
-            }
-            set
-            {
-                if (value < 0)
-                {
-                    value = 0;
-                }
-                this.length = value;
-            }
-        }
+        public double Length { get; set; }
         public List<string> Coordinates { get; set; }
         public string Season { get; set; }
-
-
 
         public Trail CreateTrail(string Id, string Length, int _Season, string Name = "Default Trail Name")
         {
             SeasonEnum temp = SeasonEnum.Summer;
-            
+
             switch (_Season)
             {
                 case 0:
                     temp = SeasonEnum.Summer;
                     break;
 
-                case 1: 
+                case 1:
                     temp = SeasonEnum.Autumn;
                     break;
 
