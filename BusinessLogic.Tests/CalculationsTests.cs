37307f880800ff57160f06a5237176e881ee0bab
ISadauskas <ignas.sadauskas7@gmail.com>
2021-11-29 13:39:16 +0200

    More unit tests
    Added units tests for calculations and Trail classes

---
+++ BusinessLogic.Tests/CalculationsTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BusinessLogic;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace BusinessLogic.Tests
+{
+    public class CalculationsTests
+    {
+        private UserJson _userJson = new UserJson();
+        private SignInJson _signInJson = new SignInJson();
+        private Calculations mockCalc = new Calculations();
+
+        [Fact]
+        public void GetIndex_ShouldReturnOne()
+        {
+            //arrange
+            int expected = 0;
+            BindingList<User> userlist = new BindingList<User>();
+            userlist.Add(new User
+            {
+                Username = "Gedas",
+                Email = "gp@123.com",
+                Password = "Admin123$",
+                Weight = 0.0,
+                RunRecord = new BindingList<Runs>(), 
+                IsAdmin = false
+            });
+
+            User target = new User
+            {
+                Username = "Gedas",
+                Email = "gp@123.com",
+                Password = "Admin123$",
+                Weight = 0.0,
+                RunRecord = new BindingList<Runs>(),
+                IsAdmin = false
+            };
+
+            //act
+            int actual = mockCalc.GetIndex(userlist, target);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AdjustUserWeight_ShouldReturnUser()
+        {
+            //arrange
+            User expected = new User
+            {
+                Username = "Gedas",
+                Email = "gp@123.com",
+                Password = "Admin123$",
+                Weight = 5.0,
+                RunRecord = new BindingList<Runs>(),
+                IsAdmin = false
+            };
+
+            var exp = JsonConvert.SerializeObject(expected);
+
+            //act
+            User actual = new User
+            {
+                Username = "Gedas",
+                Email = "gp@123.com",
+                Password = "Admin123$",
+                Weight = 0.0,
+                RunRecord = new BindingList<Runs>(),
+                IsAdmin = false
+
+            };
+            actual = mockCalc.AdjustUserWeight(actual, "5,0");
+
+            var act = JsonConvert.SerializeObject(actual);
+
+            //assert
+            Assert.Equal(exp, act); 
+        }
+
+        [Fact]
+        public void EditUser_ShouldReturnTrue()
+        {
+            //arrange
+
+            //act
+            bool actual = mockCalc.EditUser("5,0");
+
+            //assert
+            Assert.True(actual);
+        }
+
+
+    }
+}
--- BusinessLogic.Tests/TrailTests.cs
+++ BusinessLogic.Tests/TrailTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,35 +9,95 @@ using EncounterMeWF;
 using Newtonsoft.Json;
 using Xunit;
 
-//TODO: figure out how to make DateTime work with these unit tests
-
 namespace BusinessLogic.Tests
 {
     public class TrailTests
     {
+
         [Fact]
         public void CreateTrail_ShouldReturnTrail()
         {
             //arrange
+            SignInJson _signInJson = new SignInJson();
             Trail expected = new Trail();
             expected.Name = "a";
             expected.Length = 2;
-            expected.Timestamp = DateTime.Now;
+            expected.Timestamp = DateTime.MaxValue;
             expected.Location = "b";
-            expected.Organizer = "c";
+            expected.Organizer = "Gedas";
 
             var exp = JsonConvert.SerializeObject(expected);
 
             //act
             Trail actual = new Trail();
-            actual = actual.CreateTrail("2", DateTime.Now.Date, DateTime.Now, "b", "a");
+            actual = actual.CreateTrail("2", DateTime.MaxValue, DateTime.MaxValue, "b", "a");
 
             var act = JsonConvert.SerializeObject(actual);
 
             //assert
-            Assert.Equal(expected, actual);
+            Assert.Equal(exp, act);
+        }
 
+
+        //TODO
+        [Fact]
+        public void ModifyTrail_ShouldReturnModifiedTrail()
+        {
+            //arrange
+            SignInJson _signInJson = new SignInJson();
+
+            Trail expected = new Trail();
+            expected.Name = "a";
+            expected.Length = 2;
+            expected.Timestamp = DateTime.MaxValue;
+            expected.Location = "b";
+            expected.Organizer = "Gedas";
+
+            Trail test = new Trail();
+            test.Organizer = "Gedas";
+
+            var exp = JsonConvert.SerializeObject(expected);
+
+            //act
+            Trail actual = new Trail();
+            actual = actual.ModifyTrail("2", DateTime.MaxValue, DateTime.MaxValue, "b", test, "a");
+            var act = JsonConvert.SerializeObject(actual);
+
+            //assert
+            Assert.Equal(exp, act);
+        }
+
+        [Fact]
+        public void UpdateTrailList_ShouldReturnUpdatedTrailList()
+        {
+            Trail test = new Trail();   
             
+            //arrange
+            BindingList<Trail> expected = new BindingList<Trail>();
+            expected.Add(new Trail
+            {
+                Name = "a",
+                Length = 25.5,
+                Timestamp = DateTime.MaxValue,
+                Location = "Vilnius",
+                Organizer = "Petras"
+            });
+
+            expected.Add(new Trail
+            {
+                Name = "b",
+                Length = 35.5,
+                Timestamp = DateTime.MaxValue,
+                Location = "Kaunas",
+                Organizer = "Vilius"
+            });
+
+            //act
+            BindingList<Trail> actual = new BindingList<Trail>();
+            actual = test.UpdateTrailList(expected);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
