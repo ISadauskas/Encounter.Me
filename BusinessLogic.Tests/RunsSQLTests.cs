ec8df37e68c73c1ea72590ae4f08d52ddc67bd63
ISadauskas <ignas.sadauskas7@gmail.com>
2021-11-30 15:23:49 +0200

    Fixed bugs

---
+++ BusinessLogic.Tests/RunsSQLTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BusinessLogic;
+using System.Data;
+
+namespace BusinessLogic.Tests
+{
+    public class RunsSQLTests
+    {
+        RunsSQL test = new RunsSQL();
+        public string ConnectionString = "Data Source=encountermedbserver.database.windows.net;Initial Catalog=EncounterMeDb;User ID=Adminas1;Password=Password1";
+        public int index = 0;
+
+        //TODO: finish figuring out testing with Datatables
+
+        [Fact]
+        public void UpdateTable_ShouldReturnDataTable()
+        {
+            //arrange
+            DataTable expected = new DataTable();
+            DataRow expectedRow;
+            expectedRow = expected.NewRow();
+
+            //act
+            DataTable actual = test.UpdateTable("Gedas");
+
+            //assert
+            Assert.Equal(expected, actual);
+
+            foreach (DataRow row in actual.Rows)
+            {
+                Assert.Equal(row, actual.Rows);
+            }
+        }
+
+        [Fact]
+        public void FindMostBurnedCalories_ShouldReturnOneTwoEightOne()
+        {
+            //arrange
+            int expected = 1281;
+
+            //act
+            int actual = test.FindMostBurnedCalories("Gedas");
+
+            //assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void FindAllBurnedCalories_ShouldReturnOneTwoEightOne()
+        {
+            //arrange
+            int expected = 1281;
+
+            //act
+            int actual = test.FindMostBurnedCalories("Gedas");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindLongestPace_ShouldReturnTen()
+        {
+            //arrange
+            double expected = 10;
+
+            //act
+            double actual = test.FindLongestPace("Gedas", "Run");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindLongestPace_ShouldReturn()
+        {
+            //arrange
+            double expected = 0;
+
+            //act
+            double actual = test.FindLongestPace("Gedas", "Walk");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ BusinessLogic.Tests/SignUpSignInUCRegexTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BusinessLogic;
+
+namespace BusinessLogic.Tests
+{
+    public class SignUpSignInUCRegexTest
+    {
+        SignUpSignInUCRegex test = new SignUpSignInUCRegex();
+
+        [Fact]
+        public void Check_ShouldReturnZero()
+        {
+            //arrange
+            int expected = 0;
+
+            //act
+            int actual = test.Check("Nirino", "Jonas@gmail.com", "Admin123?", "Admin123?");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Check_ShouldReturnOne()
+        {
+            //arrange
+            int expected = 1;
+
+            //act
+            int actual = test.Check("", "Jonas@gmail.com", "Admin123?", "Admin123?");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Check_ShouldReturnTwo()
+        {
+            //arrange
+            int expected = 2;
+
+            //act
+            int actual = test.Check("Admin123", "Jonas@gmail.com", "Admin123?", "Admin123?");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Check_ShouldReturnThree()
+        {
+            //arrange
+            int expected = 3;
+
+            //act
+            int actual = test.Check("Nirino", "", "Admin123?", "Admin123?");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Check_ShouldReturnFour()
+        {
+            //arrange
+            int expected = 4;
+
+            //act
+            int actual = test.Check("Nirino", "Jonas@gmai", "Admin123?", "Admin123?");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Check_ShouldReturnFive()
+        {
+            //arrange
+            int expected = 5;
+
+            //act
+            int actual = test.Check("Nirino", "gepr@yahoo.net", "Admin123?", "Admin123?");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Check_ShouldReturnSix()
+        {
+            //arrange
+            int expected = 6;
+
+            //act
+            int actual = test.Check("Nirino", "Jonas@gmail.com", "", "Admin123?");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Check_ShouldReturnSeven()
+        {
+            //arrange
+            int expected = 7;
+
+            //act
+            int actual = test.Check("Nirino", "Jonas@gmail.com", "sdsadsa", "Admin123?");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Check_ShouldReturnEight()
+        {
+            //arrange
+            int expected = 8;
+
+            //act
+            int actual = test.Check("Nirino", "Jonas@gmail.com", "Admin123?", "Admin");
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+
+
+}
